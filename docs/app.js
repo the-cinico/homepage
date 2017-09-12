a9ba23dfc2727aa8ee9a783ea1c3ea4109bf9827
Liyakhath Syed <fireflyliya@gmail.com>
2017-09-12 16:19:29 -0400

    done

---
+++ docs/app.js
@@ -0,0 +1,12 @@
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker
+    .register('../service-worker.js', { scope: './' })
+    .then(function(registration) {
+      alert("Service Worker Registered");
+    })
+    .catch(function(err) {
+      alert("Service Worker Failed to Register", err);
+    })
+
+}
